f74695d938b71f24d9062ce6e276eaac3f91ad19
David Yoon <qqpo12@naver.com>
2024-10-26 21:13:27 +0900

    Add markers
    * Added markers on the map
    * Used the cameraUpdate to move position on first item of the result list

--- app/src/main/java/com/example/fcmapapp/MainActivity.kt
+++ app/src/main/java/com/example/fcmapapp/MainActivity.kt
@@ -2,17 +2,22 @@ package com.example.fcmapapp
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.fcmapapp.databinding.ActivityMainBinding
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.Tm128
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -33,14 +38,58 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding.mapView.getMapAsync(this)
 
-        SearchRepository.getGoodRestaurants("서울").enqueue(object : Callback<SearchResult> {
-            override fun onResponse(p0: Call<SearchResult>, p1: Response<SearchResult>) {
-                Log.e("aa", "${p1.body().toString()}")
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return if (query?.isNotEmpty() == true) {
+                    SearchRepository.getGoodRestaurants(query).enqueue(object : Callback<SearchResult> {
+                        override fun onResponse(p0: Call<SearchResult>, p1: Response<SearchResult>) {
+                            val searchItemList = p1.body()?.items.orEmpty()
 
+                            if (searchItemList.isEmpty()) {
+                                Toast.makeText(this@MainActivity, "검색 결과가 없습니다.", Toast.LENGTH_SHORT).show()
+                                return
+                            } else if (isMapInit.not()) {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "오류가 발생했습니다.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                return
+                            }
+
+                            Log.e("MainActivity", searchItemList.toString())
+                            val markers = searchItemList.map {
+                                Marker().apply {
+                                    position = LatLng((it.mapy.toDouble() / 10000000), (it.mapx.toDouble() / 10000000))
+                                    captionText = it.title
+                                    map = naverMap
+                                }
+                            }
+
+
+                            Log.e("MainActivity", markers.first().position.toString())
+
+
+
+                            val cameraUpdate = CameraUpdate.scrollTo(markers.first().position)
+                                .animate(CameraAnimation.Easing)
+
+                            naverMap.moveCamera(cameraUpdate)
+
+                        }
+
+                        override fun onFailure(p0: Call<SearchResult>, p1: Throwable) {
+                            p1.printStackTrace()
+                        }
+                    })
+                   false
+                } else {
+                    true
+                }
             }
 
-            override fun onFailure(p0: Call<SearchResult>, p1: Throwable) {
-                p1.printStackTrace()
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
             }
         })
 
@@ -87,13 +136,5 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(map: NaverMap) {
         naverMap = map
         isMapInit = true
-
-        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5666102, 126.9783881))
-            .animate(CameraAnimation.Easing)
-        naverMap.moveCamera(cameraUpdate)
-
-        val marker = Marker()
-        marker.position = LatLng(37.5670135, 126.9783740)
-        marker.map = naverMap
     }
 }
